c24bc01e1ab5335f294a191a48804e405e406593
iwataf <iwata@pickle.ne.jp>
2019-01-26 16:54:09 +0900

    フェード範囲調整

--- NoBalance/NoBalance/Assets/Scripts/Title/Fader.cs
+++ NoBalance/NoBalance/Assets/Scripts/Title/Fader.cs
@@ -42,7 +42,7 @@ public class Fader : MonoBehaviour
 		fadeImage.transform.SetParent(fadeCanvas.transform, false);
 		fadeImage.rectTransform.anchoredPosition = Vector3.zero;
 
-		fadeImage.rectTransform.sizeDelta = new Vector2(900, 500);
+		fadeImage.rectTransform.sizeDelta = new Vector2(2000, 800);
 	}
 
 	//フェードイン開始
