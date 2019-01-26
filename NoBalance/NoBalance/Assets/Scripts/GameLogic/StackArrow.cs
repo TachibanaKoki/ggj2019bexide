5fb2b77378219fc6528f9da1f33eded21c5dc2a8
tachibanakoki <standchat0820@gmail.com>
2019-01-26 09:49:22 +0900

    add gamelogic

---
+++ NoBalance/NoBalance/Assets/Scripts/GameLogic/StackArrow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum OrderType
+{
+    Up,
+    Down,
+    Right,
+    Left
+}
+
+
+public class StackOrder : MonoBehaviour
+{
+    public List<OrderType> _rightOrderTypes { get; private set; } = new List<OrderType>();
+    public List<OrderType> _LeftOrderTypes { get; private set; } = new List<OrderType>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
